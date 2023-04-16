5a68bfe31219ae2f0ccb289614e4945a016ea845
Artur Kryszak <artur.kryszak@sonalake.com>
2023-04-16 11:55:57 +0200

    Demo controller

---
+++ demo-swagger/src/main/java/pl/coderslab/demoswagger/DemoController.java
@@ -0,0 +1,13 @@
+package pl.coderslab.demoswagger;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello";
+    }
+}
